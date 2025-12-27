a6245d3a0705c55a3d91263f57cdd55e23482d1a
Sasha Radionov <workmail.radionov@gmail.com>
2025-12-27 06:08:23 -0700

    EnemyAuthoring + followingSystem
    EnemyAuthoring file was created and added to an enemy prefab
    
    PlayerFollowSystem makes every enemy with this authoring to follow a player with constant speed(some changes requiered(+comments), enemies get stuck on environment objects, some pathfinding system has to be created)

--- Assets/Prefabs/EnemyAuthoring.cs
+++ Assets/Prefabs/EnemyAuthoring.cs
@@ -1,15 +1,39 @@
+using Unity.Burst;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.VisualScripting;
 using UnityEngine;
+
+public struct EnemyTag : IComponentData { }
 
 class EnemyAuthoring : MonoBehaviour
 {
-    
+    class EnemyAuthoringBaker : Baker<EnemyAuthoring>
+    {
+        public override void Bake(EnemyAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent<EnemyTag>(entity);
+        }
+    }
 }
 
-class EnemyAuthoringBaker : Baker<EnemyAuthoring>
+[BurstCompile]
+[UpdateAfter(typeof(TransformSystemGroup))]
+public partial struct PlayerFollowSystem : ISystem
 {
-    public override void Bake(EnemyAuthoring authoring)
+    public void OnUpdate(ref SystemState state)
     {
-        
+        foreach (var playerPos in SystemAPI.Query<LocalToWorld>().WithAll<PlayerTag>())
+        {
+            foreach (var (enemyPos, enemyDirection) in SystemAPI.Query<LocalToWorld, RefRW<CharacterMoveDirection>>().WithAll<EnemyTag>()) //dont nest queries, store player pos once and re-use it
+            {
+                float x = playerPos.Position.x - enemyPos.Position.x; //float2-float2 (update)
+                float y = playerPos.Position.y - enemyPos.Position.y;
+
+                enemyDirection.ValueRW.Value = math.normalizesafe(new float2 (x, y));
+            }
+        }
     }
 }
